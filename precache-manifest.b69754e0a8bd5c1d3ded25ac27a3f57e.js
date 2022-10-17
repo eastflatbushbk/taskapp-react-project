8f50393391cc3e6768f47838443c00aad5928b25
Robert Sainton <robertsainton@outlook.com>
2022-10-17 15:20:54 -0400

    Updates

---
+++ precache-manifest.b69754e0a8bd5c1d3ded25ac27a3f57e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a08b0bc8b44903f7472",
+    "url": "/taskapp-react-project/static/css/main.b78381ed.chunk.css"
+  },
+  {
+    "revision": "2a08b0bc8b44903f7472",
+    "url": "/taskapp-react-project/static/js/main.e4780216.chunk.js"
+  },
+  {
+    "revision": "766a2ee14ba6a215813c",
+    "url": "/taskapp-react-project/static/js/runtime~main.324d3bc5.js"
+  },
+  {
+    "revision": "f2a44fd62e98df8b7b09",
+    "url": "/taskapp-react-project/static/js/2.c7bb05a6.chunk.js"
+  },
+  {
+    "revision": "60cf4ecf76dd825d6b0e",
+    "url": "/taskapp-react-project/static/js/3.b84871a5.chunk.js"
+  },
+  {
+    "revision": "cd024bf162de1ea95ce21d80c91ff767",
+    "url": "/taskapp-react-project/index.html"
+  }
+];
